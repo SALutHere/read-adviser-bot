12f63b9d01ed50c8736be087dbc919a4153c06b8
SALutHere <leetmansup@gmail.com>
2025-10-23 21:04:03 +0300

    Minor fixes

--- events/telegram/messages.go
+++ events/telegram/messages.go
@@ -1,13 +1,13 @@
 package telegram
 
-const msgHelp = `I can save and keep your pages. Also I can offer you to them to read.
+const msgHelp = `I can save and keep your pages. Also I can offer you them to read.
 
 In order to save the page, just send me a link to it.
 
-In order to get a random page from your list, send me command /rnd.
+In order to get a random page from your list, send me a command /rnd.
 Caution! After that, this page will be removed from your list!`
 
-const msgHello = "He there! \n\n" + msgHelp
+const msgHello = "Hi there! \n\n" + msgHelp
 
 const (
 	msgUnknownCommand = "Unknown command 🤔"
